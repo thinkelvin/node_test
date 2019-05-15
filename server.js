f4287840c70e06add458ac56b35a2bafe9915159
Kelvin Lee <thinkelvin@gmail.com>
2019-05-15 17:33:34 +0800

    no default port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen();
 app.use(express.static('public'));
 console.log("my socket server is running at port:3000");
