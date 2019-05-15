0c33b1ff21571af3a7e5c1b3ca7048358951c27f
Kelvin Lee <thinkelvin@gmail.com>
2019-05-15 17:46:57 +0800

    revision for heroku to work

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
-var server = app.listen();
+var port= process.env.PORT || 8080;
+var server = app.listen(port, function() {
+    console.log('listening at:'+port);
+});
 app.use(express.static('public'));
+app.get('/', function(req, res){
+    res.render('index');
+});
 console.log("my socket server is running at port:3000");
